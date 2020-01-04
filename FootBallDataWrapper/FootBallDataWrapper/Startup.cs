a6c314605e5c69cce2aa1aa9a5c7239e3d6cf1e5
Julian Slonim <slonim.julian@gmail.com>
2020-01-04 00:35:31 -0300

    nswag

--- FootBallDataWrapper/FootBallDataWrapper/Startup.cs
+++ FootBallDataWrapper/FootBallDataWrapper/Startup.cs
@@ -26,6 +26,8 @@ namespace FootBallDataWrapper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOpenApiDocument();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,9 @@ namespace FootBallDataWrapper
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseEndpoints(endpoints =>
             {

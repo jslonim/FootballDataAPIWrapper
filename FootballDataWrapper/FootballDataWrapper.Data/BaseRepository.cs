63f3e933a12195caf0149e60a18403b9b9edfc14
Julian Slonim <slonim.julian@gmail.com>
2020-01-04 01:29:57 -0300

    Data nullified

--- FootballDataWrapper/FootballDataWrapper.Data/BaseRepository.cs
+++ FootballDataWrapper/FootballDataWrapper.Data/BaseRepository.cs
@@ -6,48 +6,48 @@ using System.Text;
 
 namespace FootballDataWrapper.Data
 {
-    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    public class BaseRepository//<T> : IBaseRepository<T> where T : class
     {
-        private readonly DbSet<T> _entities;
+        //private readonly DbSet<T> _entities;
 
-        public BaseRepository(DbContext context)
-        {
-            _entities = context.Set<T>();
-        }
+        //public BaseRepository(DbContext context)
+        //{
+        //    _entities = context.Set<T>();
+        //}
 
-        public T GetById(int id)
-        {
-            return _entities.Find(id);
-        }
+        //public T GetById(int id)
+        //{
+        //    return _entities.Find(id);
+        //}
 
-        public IEnumerable<T> GetAll()
-        {
-            return _entities.ToList();
-        }
+        //public IEnumerable<T> GetAll()
+        //{
+        //    return _entities.ToList();
+        //}
 
-        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
-        {
-            return _entities.Where(predicate);
-        }
+        //public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        //{
+        //    return _entities.Where(predicate);
+        //}
 
-        public void Add(T entity)
-        {
-            _entities.Add(entity);
-        }
+        //public void Add(T entity)
+        //{
+        //    _entities.Add(entity);
+        //}
 
-        public void AddRange(IEnumerable<T> entities)
-        {
-            _entities.AddRange(entities);
-        }
+        //public void AddRange(IEnumerable<T> entities)
+        //{
+        //    _entities.AddRange(entities);
+        //}
 
-        public void Remove(T entity)
-        {
-            _entities.Remove(entity);
-        }
+        //public void Remove(T entity)
+        //{
+        //    _entities.Remove(entity);
+        //}
 
-        public void RemoveRange(IEnumerable<T> entities)
-        {
-            _entities.RemoveRange(entities);
-        }
+        //public void RemoveRange(IEnumerable<T> entities)
+        //{
+        //    _entities.RemoveRange(entities);
+        //}
     }
 }
